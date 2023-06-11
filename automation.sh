4662efcc70b05653130cbcbe9b5fabdc0c87271a
Nisha Nimesh <nishanimesh@yahoo.com>
2023-06-12 01:31:31 +0530

    file added

---
+++ automation.sh
@@ -0,0 +1,13 @@
+sudo apt update -y
+sudo apt install apache2
+sudo service apache2 start
+sudo service apache2 status 
+timestamp=$(date '+%d%m%Y-%H%M%S')  
+sudo tar cvf "Nisha-httpd-logs${timestamp}.tar" "ubuntu\var\log\apache2\access"
+
+myname=nisha
+s3_bucket=upgrad-nishanimesh
+timestamp=$(date '+%d%m%Y-%H%M%S') 
+aws s3 \
+cp /tmp/${myname}-httpd-logs${timestamp}.tar \
+s3://${s3_bucket}/${myname}-httpd-logs${timestamp}.tar
